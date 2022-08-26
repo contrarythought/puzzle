67eb9d4431aceb0a50816a28df880a9a335af33f
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-08-26 14:48:54 -0400

    Update solve_lib.go

--- solve/solve_lib.go
+++ solve/solve_lib.go
@@ -1,5 +1,15 @@
 package solve
 
-func Solve(diagram []byte) {
+const (
+	MAX_CHAR = 255
+)
 
+// TODO
+func Solve(diagram []byte) {
+	char_chan := make(chan string, 1)
+	for i := 0; i < MAX_CHAR; i++ {
+		go func(i int) {
+
+		}(i)
+	}
 }
