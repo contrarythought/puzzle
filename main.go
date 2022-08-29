43aaf342fe9b3e565947ee70f6c4fe39660ab0a3
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-08-29 18:03:24 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,27 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"nymin/lib_dict"
 	"nymin/solve"
 	"os"
 )
 
-func AddToDict(dict_buf []byte, dictionary *lib_dict.Dictionary) {
-	var tmp_buf []byte
-	for _, char := range string(dict_buf) {
-		if string(char) == "\n" {
-			dictionary.Add(string(tmp_buf))
-			tmp_buf = nil
-		} else {
-			tmp_buf = append(tmp_buf, byte(char))
-		}
-	}
-}
-
 func main() {
-	buffer, err := os.ReadFile("diagram2.txt")
+	buffer, err := os.ReadFile("diagram.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,17 +19,11 @@ func main() {
 	}
 
 	dictionary := lib_dict.NewDictionary()
-	AddToDict(dict_buf, dictionary)
+	dictionary.AddToDict(dict_buf)
 
 	wordRepo := lib_dict.NewWordRepo()
 	solve.Solve(string(buffer), wordRepo, dictionary)
 
 	// prints out all words in the picture
 	wordRepo.PrintWordsJSON()
-
-	if dictionary.Has("shapable") {
-		fmt.Println("HAS SAUCE")
-	} else {
-		fmt.Println("NO SAUCE")
-	}
 }
