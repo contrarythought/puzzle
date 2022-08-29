9dac82518b9d9fb752dc9dcbaddf78dbbf202e8e
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-08-28 21:52:21 -0400

    Update solve_lib.go

--- solve/solve_lib.go
+++ solve/solve_lib.go
@@ -1,18 +1,21 @@
 package solve
 
 import (
+	"fmt"
 	"nymin/lib_dict"
+	"strings"
 	"sync"
 )
 
 const (
-	MAX_CHAR = 255
+	// CHANGE TO 255
+	MAX_BOUNDARY = 15
 )
 
 // TODO
 func Solve(diagram string, wp *lib_dict.WordRepo, d *lib_dict.Dictionary) {
 	var wg sync.WaitGroup
-	for i := 0; i < MAX_CHAR; i++ {
+	for i := 0; i < MAX_BOUNDARY; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
@@ -35,34 +38,35 @@ func Solve(diagram string, wp *lib_dict.WordRepo, d *lib_dict.Dictionary) {
 }
 
 func AnalyzeFront(index int, diagram string, wp *lib_dict.WordRepo, d *lib_dict.Dictionary, word []rune) {
-	if index < 0 || index >= MAX_CHAR {
+	if index < 0 || index >= MAX_BOUNDARY {
 		return
 	}
 	if (index+1)%15 == 0 {
 		word = append(word, rune(diagram[index]))
-		wp.Add(string(word), d)
+		wp.Add(strings.TrimSpace(string(word)), d)
 		index++
 		return
 	} else {
 		word = append(word, rune(diagram[index]))
-		wp.Add(string(word), d)
+		fmt.Println(string(word))
+		wp.Add(strings.TrimSpace(string(word)), d)
 		index++
 		AnalyzeFront(index, diagram, wp, d, word)
 	}
 }
 
 func AnalyzeBack(index int, diagram string, wp *lib_dict.WordRepo, d *lib_dict.Dictionary, word []rune) {
-	if index < 0 || index >= MAX_CHAR {
+	if index < 0 || index >= MAX_BOUNDARY {
 		return
 	}
 	if index%15 == 0 {
 		word = append(word, rune(diagram[index]))
-		wp.Add(string(word), d)
+		wp.Add(strings.TrimSpace(string(word)), d)
 		index--
 		return
 	} else {
 		word = append(word, rune(diagram[index]))
-		wp.Add(string(word), d)
+		wp.Add(strings.TrimSpace(string(word)), d)
 		index--
 		AnalyzeBack(index, diagram, wp, d, word)
 	}
@@ -70,42 +74,42 @@ func AnalyzeBack(index int, diagram string, wp *lib_dict.WordRepo, d *lib_dict.D
 
 // TODO
 func AnalyzeVertLR(index int, diagram string, wp *lib_dict.WordRepo, d *lib_dict.Dictionary, word []rune) {
-	if index < 0 || index >= MAX_CHAR {
+	if index < 0 || index >= MAX_BOUNDARY {
 		return
 	}
 	word = append(word, rune(diagram[index]))
-	wp.Add(string(word), d)
+	wp.Add(strings.TrimSpace(string(word)), d)
 	index += 16
 	AnalyzeVertLR(index, diagram, wp, d, word)
 }
 
 // TODO
 func AnalyzeVertRL(index int, diagram string, wp *lib_dict.WordRepo, d *lib_dict.Dictionary, word []rune) {
-	if index < 0 || index >= MAX_CHAR {
+	if index < 0 || index >= MAX_BOUNDARY {
 		return
 	}
 	word = append(word, rune(diagram[index]))
-	wp.Add(string(word), d)
+	wp.Add(strings.TrimSpace(string(word)), d)
 	index -= 16
 	AnalyzeVertRL(index, diagram, wp, d, word)
 }
 
 func AnalyzeDown(index int, diagram string, wp *lib_dict.WordRepo, d *lib_dict.Dictionary, word []rune) {
-	if index < 0 || index >= MAX_CHAR {
+	if index < 0 || index >= MAX_BOUNDARY {
 		return
 	}
 	word = append(word, rune(diagram[index]))
-	wp.Add(string(word), d)
+	wp.Add(strings.TrimSpace(string(word)), d)
 	index += 15
 	AnalyzeDown(index, diagram, wp, d, word)
 }
 
 func AnalyzeUp(index int, diagram string, wp *lib_dict.WordRepo, d *lib_dict.Dictionary, word []rune) {
-	if index < 0 || index >= MAX_CHAR {
+	if index < 0 || index >= MAX_BOUNDARY {
 		return
 	}
 	word = append(word, rune(diagram[index]))
-	wp.Add(string(word), d)
+	wp.Add(strings.TrimSpace(string(word)), d)
 	index -= 15
 	AnalyzeUp(index, diagram, wp, d, word)
 }
